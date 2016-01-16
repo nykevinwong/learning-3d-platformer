e11c6ec057b1d2fb384abe44e448b997062fd507
nyckevinwong@gmail.com <nyckevinwong@gmail.com>
2016-01-16 17:46:00 -0500

    added C# script and a command batch file to build android package in Build Folder.

---
+++ Assets/Scripts/Editor/BuildAndroidScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Linq;
+
+public class BuildAndroidScript : MonoBehaviour {
+
+	public static string __build_output_folder = ".\\Build\\";
+	public static string __android_output_filename = "android.apk";
+
+		static void StartBuild()
+		{
+			string final_apk_path = __build_output_folder + __android_output_filename;
+			BuildPipeline.BuildPlayer(GetScenes(), final_apk_path, BuildTarget.Android, BuildOptions.None);
+		}
+		
+		static string[] GetScenes()
+		{
+			return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+		}
+}
---
+++ buildAndroid.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\Unity5\Editor\Unity.exe" -quit -batchmode -executeMethod BuildAndroidScript.StartBuild -projectPath "C:\Development\unity3d\3d-platformer" -logFile "unitybuild.log"
+pause
