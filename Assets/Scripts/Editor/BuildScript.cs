57726a98f32797f506e96dd9c861e86c18669f99
nyckevinwong@gmail.com <nyckevinwong@gmail.com>
2016-01-17 19:51:40 -0500

    added BuildScript for compiling WebGL build.

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Linq;
+
+public class BuildScript : MonoBehaviour {
+
+	public static string __build_output_folder = ".\\build\\";
+
+	public static string __android_output_filename = "android\\android.apk";
+	public static string __webgl_output_folder = "webgl";
+
+	static string[] GetScenes()
+	{
+		return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+	}
+
+	static void CompileAndroidBuild()
+	{
+		string final_path = __build_output_folder + __android_output_filename;
+		BuildPipeline.BuildPlayer(GetScenes(), final_path, BuildTarget.Android, BuildOptions.None);
+	}
+
+	static void CompileWebGLBuild()
+	{
+		string final_path = __build_output_folder + __webgl_output_folder;
+		BuildPipeline.BuildPlayer(GetScenes(), final_path, BuildTarget.WebGL, BuildOptions.None);
+	}
+
+
+}
+
---
+++ build.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\Unity5\Editor\Unity.exe" -quit -batchmode -executeMethod %1 -projectPath "C:\Development\unity3d\3d-platformer" -logFile "unitybuild.log"
+pause
